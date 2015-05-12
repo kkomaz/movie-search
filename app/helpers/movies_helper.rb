81fcbfb979905816e25d37736685f9ea8ef929ef
kkomaz <leeajl006@gmail.com>
2015-05-12 19:03:12 -0400

    css clean-up

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,22 @@
 module MoviesHelper
+  def watch_list(user, movie)
+    if @user.movies.include?(movie)
+      link_to 'Delete Watchlist', user_movie_path(@user, movie), confirm: 'Are you sure?', method: :delete, class: "btn btn-danger"
+    else
+      link_to user_movies_path(@user,
+                          {:movies => {
+                              :title => movie.title,
+                              :country => movie.country,
+                              :collection_price => movie.collection_price.to_s,
+                              :image => movie.image,
+                              :short_descrip => movie.short_descrip,
+                              :long_descrip => movie.long_descrip
+                            }}),
+                          class: "btn btn-success",
+                          :method => :post do
+      "Save to Watchlist"
+    end
+    end
+  end
+
 end

3e115d20fa9a3e94dd2bbe763efa3cda6e59627d
kkomaz <leeajl006@gmail.com>
2015-05-13 10:02:45 -0400

    convert back to sqlite3 from pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150512174308) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "movies", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -43,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150512174308) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["auth_token"], name: "index_users_on_auth_token", using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token"
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
